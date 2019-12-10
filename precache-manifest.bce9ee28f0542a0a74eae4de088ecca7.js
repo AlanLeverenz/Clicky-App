52874e39a7788c19564e934400ed8709107fbb98
Alan Leverenz <awleverenz@aol.com>
2019-12-10 15:17:15 -0500

    Updates

---
+++ precache-manifest.bce9ee28f0542a0a74eae4de088ecca7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2614756ed6e93609adff8252bcefac8f",
+    "url": "/Clicky-App/index.html"
+  },
+  {
+    "revision": "d9c807d4b4370a34330c",
+    "url": "/Clicky-App/static/css/main.d2473386.chunk.css"
+  },
+  {
+    "revision": "88b96804cd8ec422b9f1",
+    "url": "/Clicky-App/static/js/2.57939245.chunk.js"
+  },
+  {
+    "revision": "d9c807d4b4370a34330c",
+    "url": "/Clicky-App/static/js/main.6c3599dd.chunk.js"
+  },
+  {
+    "revision": "d0690000752e553ae8fb",
+    "url": "/Clicky-App/static/js/runtime-main.f09b89e7.js"
+  },
+  {
+    "revision": "86c82c7a19b4376b0cfa2b77f0653b8a",
+    "url": "/Clicky-App/static/media/bg.86c82c7a.png"
+  },
+  {
+    "revision": "2b84948f7aee92f994f1e1652d3cb735",
+    "url": "/Clicky-App/static/media/c-3po.2b84948f.jpg"
+  },
+  {
+    "revision": "15541105d6369e07e54c68a226ed2009",
+    "url": "/Clicky-App/static/media/count-dooku.15541105.jpg"
+  },
+  {
+    "revision": "a7fd83e74827a3cadf79941d692771cc",
+    "url": "/Clicky-App/static/media/darth-maul.a7fd83e7.jpg"
+  },
+  {
+    "revision": "08f91023c969637ce1bdfbcdda1b5381",
+    "url": "/Clicky-App/static/media/general-grevious.08f91023.jpg"
+  },
+  {
+    "revision": "2ffde15a711cfb0bf29b39fad18bd042",
+    "url": "/Clicky-App/static/media/greedo.2ffde15a.jpg"
+  },
+  {
+    "revision": "af970340db20e985c7d5ff5f04fe735d",
+    "url": "/Clicky-App/static/media/maz_kanata.af970340.jpg"
+  },
+  {
+    "revision": "4e25b585616a93c7f9b0666afd9ed273",
+    "url": "/Clicky-App/static/media/padme-amidala.4e25b585.jpg"
+  },
+  {
+    "revision": "dda55816909f3d3cd84d52f97d240b3b",
+    "url": "/Clicky-App/static/media/poe-dameron.dda55816.jpg"
+  },
+  {
+    "revision": "9376200df092b634352458c13b035b8b",
+    "url": "/Clicky-App/static/media/rey.9376200d.jpg"
+  },
+  {
+    "revision": "f9745aff6594630753bcd85eb2845ed3",
+    "url": "/Clicky-App/static/media/salacious-b-crumb.f9745aff.jpg"
+  },
+  {
+    "revision": "cc184394f57bfb5175f4964a69725536",
+    "url": "/Clicky-App/static/media/sebulba.cc184394.jpg"
+  },
+  {
+    "revision": "d863c8152b9a9e828f98165a7c5572b1",
+    "url": "/Clicky-App/static/media/tusken-raider.d863c815.jpg"
+  }
+]);
--- src/components/Footer/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Footer.css';
-
-const Footer = () => <div className="footer text-center">Alan Leverenz</div>;
-
-export default Footer;
